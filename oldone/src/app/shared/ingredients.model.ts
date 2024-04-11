8767e3331ba1835bae5ae36e2d6b390947d9298a
Rishabh Kumar <94367524+Rishabhkr1802@users.noreply.github.com>
2024-04-11 10:08:54 +0530

    Initial Commit

---
+++ oldone/src/app/shared/ingredients.model.ts
@@ -0,0 +1,14 @@
+export class Ingredients{
+    constructor(public name: string, public amount: number){
+        
+    }
+    
+    //Same as
+    /*public name : string;
+    public amount : number;
+    
+    constructor(name: string , amount:number){
+        this.name=name;
+        this.amount
+    }*/
+}
